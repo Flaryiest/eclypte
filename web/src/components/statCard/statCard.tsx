c30e2fdd919a62f7e2380b58c5ab762b395fd171
Flaryiest <ericmzuo1@gmail.com>
2026-04-15 20:06:10 -0600

    stat card

---
+++ web/src/components/statCard/statCard.tsx
@@ -0,0 +1,17 @@
+import styles from "./statCard.module.css";
+
+type Props = {
+    value: string;
+    label: string;
+    description: string;
+};
+
+export default function StatCard({ value, label, description }: Props) {
+    return (
+        <div className={styles.card}>
+            <h3 className={styles.value}>{value}</h3>
+            <p className={styles.label}>{label}</p>
+            <p className={styles.description}>{description}</p>
+        </div>
+    );
+}
