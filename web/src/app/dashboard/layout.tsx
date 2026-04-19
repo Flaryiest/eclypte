3735c0a35ff07ab2cb5c86bed33ce29b478f2fc4
Flaryiest <ericmzuo1@gmail.com>
2026-04-18 20:24:19 -0600

    minor dashboard scaffolding

--- web/src/app/dashboard/layout.tsx
+++ web/src/app/dashboard/layout.tsx
@@ -1,4 +1,9 @@
-export default function Layout() {
-    return <div>
-    </div>
+import styles from "./layout.module.css"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return <div className={styles.container}>
+            <div className={styles.dashboard}>
+                {children}
+            </div>
+        </div>
 }
